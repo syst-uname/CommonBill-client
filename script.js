7d845954ff07828a026e59a8ee9957badfb07abe
Александр <alexxxakm@mail.ru>
2025-01-23 00:23:31 +0300

    fix: задачи без запроса, правильный формат данных при ответе

--- script.js
+++ script.js
@@ -22,16 +22,8 @@ document.addEventListener('DOMContentLoaded', function () {
   tg.MainButton.setText('Главная кнопка (отправить)');
   tg.MainButton.onClick(() => {
     console.log('==== Главная кнопка Clicked 02')
-    tg.sendData({ data: 1, text: 'Главная кнопка Clicked' });
+    tg.sendData('Данные клиента: Главная кнопка Clicked');
   });
-
-
-  tg.onEvent('mainButtonClicked', function () {
-    console.log('==== mainButtonClicked 01')
-    tg.sendData({ data: 2, text: 'mainButtonClicked ' });
-  });
-
-
 
   document.getElementById('testButton').addEventListener('click', function () {
     tg.setHeaderColor('#fcb69f')
@@ -43,28 +35,25 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
 
+  const tasks = [
+    { id: 1, title: 'Задача 1', completed: false },
+    { id: 2, title: 'Задача 2', completed: true },
+    { id: 3, title: 'Задача 3', completed: false }
+  ]
+  tasks.forEach(task => {
+    const taskCard = document.createElement('div');
+    taskCard.classList.add('col-md-4', 'task-card');
+    if (task.completed) {
+      taskCard.classList.add('completed');
+    }
 
-
-  fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(response => response.json())
-    .then(tasks => {
-      tasks.forEach(task => {
-        const taskCard = document.createElement('div');
-        taskCard.classList.add('col-md-4', 'task-card');
-        if (task.completed) {
-          taskCard.classList.add('completed');
-        }
-
-        taskCard.innerHTML = `
+    taskCard.innerHTML = `
                   <h5>${task.title}</h5>
-                  <p>Статус: ${task.completed ? 'Выполнено' : 'Не выполнено'}</p>
+                  <p>Статус: ${task.completed ? '✅' : '☑️'}</p>
                   <small>ID задачи: ${task.id}</small>
               `;
 
-        taskList.appendChild(taskCard);
-      });
-    })
-    .catch(error => {
-      console.error('Ошибка при получении задач:', error);
-    });
+    taskList.appendChild(taskCard);
+  });
+
 });
