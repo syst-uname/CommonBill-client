f93cfc817b1265e8d64e504b4eeb92ef6de57f5b
Александр <alexxxakm@mail.ru>
2025-02-15 21:44:15 +0300

    feat: отправка запроса на сервер и чтение данные, файл с конфигом, удаление ссылки

--- script.js
+++ script.js
@@ -12,39 +12,99 @@ document.addEventListener('DOMContentLoaded', function () {
     tg.sendData(JSON.stringify({ type: 'mainButtonClicked', value: '1', cost: 'data' }));
   });
 
-  document.getElementById('testButton').addEventListener('click', function () {
-    tg.setHeaderColor('#fcb69f')
-  });
-
   document.getElementById('testButton2').addEventListener('click', function () {
     tg.sendData(JSON.stringify({ type: 'secondButtonClicked', value: '2', cost: 'data new' }));
     // tg.close()
   });
 
-
-  // Список задач
-  const tasks = [
-    { id: 1, title: 'Задача 1', completed: false },
-    { id: 2, title: 'Задача 2', completed: true },
-    { id: 3, title: 'Задача 3', completed: false }
-  ]
-
-  const taskList = document.getElementById('task-list');
-
-  tasks.forEach(task => {
-    const taskCard = document.createElement('div');
-    taskCard.classList.add('col-md-4', 'task-card');
-    if (task.completed) {
-      taskCard.classList.add('completed');
-    }
-
-    taskCard.innerHTML = `
-                  <h5>${task.title}</h5>
-                  <p>Статус: ${task.completed ? '✅' : '☑️'}</p>
-                  <small>ID задачи: ${task.id}</small>
-              `;
-
-    taskList.appendChild(taskCard);
-  });
+  checkAndUpdateCache();
 
 });
+
+// Получение параметров страницы 
+async function fetchParams() {
+  try {
+    const config = await getConfig();
+    const body = {
+      type: 'getParams',
+      userId: 20
+    }
+    const response = await fetch(config.BOT_SERVER_URL, {
+      method: 'POST',
+      body: JSON.stringify(body),
+    })
+    const data = await response.json()
+    console.log('==== настройки', data)
+    return data
+  } catch (error) {
+    console.error('Ошибка при загрузке настроек:', error)
+    return null
+  }
+}
+
+// Обновление страницы по параметрам из экселя 
+function updatePage(params) {
+  try {
+    // Очищаем список перед добавлением новых данных
+    const dataList = document.getElementById('task-list');
+    dataList.innerHTML = '';
+
+    // Добавляем данные в список
+    params.currs.forEach(curr => {
+      const currencyCard = document.createElement('div');
+      currencyCard.classList.add('col-md-4');
+      currencyCard.innerHTML = `
+          <div class="card">
+            <div class="card-body">
+              <p class="card-title">${curr.id}: ${curr.title}</p>
+            </div>
+          </div>
+        `;
+      dataList.appendChild(currencyCard);
+    });
+  } catch (error) {
+    console.error('Ошибка при загрузке данных:', error);
+  }
+}
+
+// Проверка и обновления кеша
+async function checkAndUpdateCache() {
+  const cachedParams = localStorage.getItem('cachedParams');
+
+  // Если есть кешированные данные, отображаем их
+  if (cachedParams) {
+    updatePage(JSON.parse(cachedParams));
+  }
+
+  // Получаем новые данные
+  const newParams = await fetchParams();
+  if (newParams) {
+    const newParamsString = JSON.stringify(newParams);
+
+    // Если данные изменились, обновляем кеш и страницу
+    if (!cachedParams || newParamsString !== cachedParams) {
+      localStorage.setItem('cachedParams', newParamsString);
+      updatePage(newParams);
+    }
+  }
+}
+
+// Данные конфига  
+async function getConfig() {
+  try {
+    const response = await fetch('config.json')
+    const config = await response.json()
+    console.log('==== config', config)
+    return config
+  } catch (error) {
+    // чтение из env 
+
+    console.log('==== файл не считан',)
+    console.log('==== env meta', import.meta)
+    console.log('==== env env', import.meta?.env)
+    console.log('==== env key', import.meta?.env?.SECRET1)
+
+    // console.error('Ошибка при загрузке настроек:', error)
+    // return null
+  }
+}
